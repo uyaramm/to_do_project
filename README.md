7e38e9deb4074661a0576ddeca9d8e3e397a006f
Uyara Montovaneli <uyaramontovaneli@gmail.com>
2024-04-03 21:56:12 -0600

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,47 @@
-# Getting Started with Create React App
+### Activity
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+#### Objective:
 
-## Available Scripts
+Develop a responsive web page that displays a To-Do List with basic functionalities to add, remove, and mark tasks as completed.
 
-In the project directory, you can run:
+#### Requirements:
 
-### `npm start`
+The page should have a simple and clean layout, with an area to display the list of tasks and a way to add new tasks.
+Each task should have an option to be marked as completed and a button to remove it from the list.
+Tasks should be stored locally (can be in localStorage) to persist between page reloads.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## How to run this app
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+1. Clone project
+2. Navegate to the terminal to install the required libraries specified in the package.json file
+npm install
+3. Run the project:
+npm start
 
-### `npm test`
+Poject will be acessible on localhost:3000
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## ToDo List Project Set up
 
-### `npm run build`
+1- Create new project:
+npx create-react-app my-app
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+2- Install mui:
+npm install @mui/material @emotion/react @emotion/styled
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+3- Install mui icons:
+npm install @mui/icons-material
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+### Improvements
 
-### `npm run eject`
+#### Functionalities
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+1. LoginPage
+2. Create categories
+3. Link on calendar date
+4. Pagination
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+#### Tests
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+Still in the learning process of writing testes in React
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,273 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState, useEffect } from 'react';
+import Alert from '@mui/material/Alert';
+import Divider from '@mui/material/Divider';
+
 
 function App() {
+  const storedTasks = JSON.parse(localStorage.getItem('tasks'))||[];
+  //array
+  const [tasks, setTasks] = useState(storedTasks);
+  //task variable
+  const [taskDescription, setTaskDescription] = useState([""]);
+  //search task
+  const [searchDescription, setSearchDescription] = useState([])
+  //alertError
+  const [showError, setShowError] = useState(false);
+  //alertCheck
+  const [checkMessage,setCheckMessage] = useState(false);
+
+  //localstorage
+  useEffect(() => 
+  {localStorage.setItem('tasks',JSON.stringify(tasks));
+
+},[tasks]
+);
+
+  //checkbox
+  const handleToggle = (task) => () => {
+    if(task.isDone){
+      task.isDone = false
+    } 
+    else{task.isDone = true}
+    
+    const newTaskList = tasks.map ((value)=>{
+      if (value.id === task.id){
+        value.isDone = task.isDone;
+        if(task.isDone && value.id === task.id){
+          setCheckMessage(true);
+          setTimeout(()=> {
+        setCheckMessage(false);
+      },3000);
+    }
+    else{setCheckMessage(false)}
+  }
+      return value;
+    })
+    setTasks(newTaskList);
+  }; 
+
+  //delete task
+  const handleDelete = (id) =>{
+    const newTaskList = tasks.filter ((value) => value.id !== id);
+    setTasks(newTaskList);
+  }
+
+  //add task
+   function addTask() { 
+    console.log(taskDescription);
+    if(taskDescription.length === 0){
+      setShowError(true);
+      setTimeout(()=> {
+        setShowError(false);
+      },3000);
+      return;
+    }
+
+    const newId = tasks.length + 1;
+    
+    var newTask = {
+      id:newId,
+      taskDescription:taskDescription,
+      isDone:false
+    }   
+    
+    setTasks ([...tasks, newTask]); 
+    setTaskDescription("");
+   }
+
+   //search task
+   function searchTask(){
+    if (searchDescription!== ""){
+      const filteredTasks = tasks.filter(task =>
+      task.taskDescription.toLowerCase().includes(searchDescription.toLowerCase())
+    );
+      const otherTasks = tasks.filter(task =>
+      !task.taskDescription.toLowerCase().includes(searchDescription.toLowerCase())
+    );
+
+    const priorityList = [...filteredTasks, ...otherTasks];
+    setTasks(priorityList)
+    setSearchDescription("");
+    
+
+      }
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Box
+sx={{
+  p: 2,
+  display:'flex',
+  flexDirection:'column',
+  alignItems:'center', 
+  maxWidth: '600px',
+  margin: ' 40px auto',
+  paddingTop:'10px',
+  backgroundColor:'#fefefe',
+  borderRadius:'10px'
+}}
+>
+  
+ <h1 style={{ color: '#1976D2',fontSize:'30px' }}>To Do List</h1>
+
+ <Box
+ component="form"
+      sx={{
+        '& > :not(style)': { m: 2, width: '25ch' },
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        width: '100%',
+        flexWrap: 'wrap', //reponsividade
+        gap: '10px', //responsividade
+        
+      }}
+      noValidate autoComplete="off"
+ >
+
+       <TextField 
+      value={searchDescription} 
+      onChange={(e) => setSearchDescription(e.target.value)} 
+      label="Search..." 
+      sx={{ flex: '1 1 200px'}} //ajustar com a tela
+      />
+
+      <Button 
+      onClick={searchTask}
+      variant="contained"
+      sx={{ padding: '8px 16px', 
+      fontSize: '0.7rem',
+      fontWeight:'bold',
+      border:'1px solid #E4F1FE',
+      backgroundColor:'#E4F1FE', 
+      color:'#1976D2',
+      '&:hover': { 
+      color: '#E4F1FE', 
+      backgroundColor: '#1976D2',
+      },
+      }}
+      >Search
+      </Button>
+            <Divider />
+
+ </Box>
+    <p style={{ color: '#1976D2', fontSize:'17px'}}>Check if task is completed</p>
+
+      {checkMessage && <Alert severity="success">Well done! Task completed </Alert>}
+      
+    <List sx={{ width: '100%', 
+    maxWidth: 360, 
+    bgcolor: '#fefefe',
+    '&:hover': {
+          backgroundColor: '#e4f1fe'
+        }
+  }}>
+      {tasks.map((value) => {
+        const labelId = `checkbox-list-label-${value.id}`;
+
+        return (
+          <Box
+          sx={{
+            display:'flex',
+            flexDirection:'column',
+            alignItems:'center',
+          }}>
+          <ListItem
+            key={value}
+            sx={{
+    borderBottom: '1px solid #ccc', 
+    paddingBottom: '8px', 
+    marginBottom: '8px',
+  }}
+            secondaryAction={
+              <IconButton 
+              onClick={()=> handleDelete(value.id)} 
+              edge="end" 
+              sx={{ color:'#840807',
+        '&:hover': {
+          backgroundColor: '#e4f1fe'
+        }
+            }}
+              >
+                 < DeleteIcon /> 
+              </IconButton>
+            }
+            disablePadding
+          >
+            <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+              <ListItemIcon sx={{ color: '#840807' }}>
+                <Checkbox
+                  edge="start"
+                  checked={value.isDone}
+                  tabIndex={-1}
+                  disableRipple
+                  sx={{ 
+          '&:hover': {
+            backgroundColor: '#e4f1fe'
+          }
+        }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`${value.taskDescription}`} />
+            </ListItemButton>
+          </ListItem>
+          
+          </Box>
+        );
+      })}
+      
+    </List>
+          
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 2, width: '25ch' },
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        width: '100%',
+        flexWrap: 'wrap', //reponsividade
+        gap: '10px', //responsividade
+        
+      }}
+      noValidate autoComplete="off"
+    >
+      <TextField 
+      value={taskDescription} 
+      onChange={(e) => setTaskDescription(e.target.value)}
+      label="Add a new task here!" 
+      sx={{ flex: '1 1 200px'}} //ajustar com a tela
+      />
+      
+      <Button 
+      onClick={addTask} 
+      variant="contained" 
+      size='small' 
+      sx={{ padding: '8px 16px', fontSize: '0.7rem',fontWeight:'bold',border:'1px solid #E4F1FE',backgroundColor:'#E4F1FE', color:'#1976D2',
+      '&:hover': { 
+      color: '#E4F1FE', 
+      backgroundColor: '#1976D2',
+      },
+      }}
+      >ADD
+      </Button>
+       {showError && <Alert severity="error">Please add a task.</Alert>}
+    </Box>
+      </Box>
+      
   );
 }
 
